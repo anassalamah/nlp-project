34d4edfc14cafff3caa4faecb3714cfb49bd1f89
Anas Salamah <anassalamah@Anass-iMac.local>
2014-12-14 13:23:05 -0700

    pushing to try to run code in the lab

--- linear regressor/merge_wiki_long_and_features_train.py
+++ linear regressor/merge_wiki_long_and_features_train.py
@@ -13,23 +13,28 @@ if __name__ == "__main__":
     # Create a File for output
     output = DictWriter(open('wiki_long_features_train.csv', 'w'), ['Question ID', 'Sentence Position', 'Answer', 'is correct', 'Q score','IR score','join score', 'category','Wikipedia Score', 'Wikipedia Found', 'SearchWords','SearchFound', 'WikipediaLen'], lineterminator='\n')
     output.writeheader()
-
+    count = 0
     for ii in wiki_long:
+    	count += 1
+    	print count
         for bb in features_train:
+            #print bb
             if (ii['Question ID'],ii['Sentence Position'],ii['Answer']) == ( bb['Question ID'],bb['Sentence Position'],bb['Answer']):
                 output.writerow({'Question ID': ii['Question ID'], \
-                                 'Sentence Position': ii['Sentence Position'], \
-                                 'Answer': ii['Answer'], \
-                                 'is correct': bb['is correct'], \
-                                 'Q score': bb['Q score'], \
-                                 'IR score': bb['IR score'], \
-                                 'join score': bb['join score'], \
-                                 'category': bb['category'], \
-                                 'Wikipedia Score': ii['Wikipedia Score'], \
-                                 'Wikipedia Found': ii['Wikipedia Found'], \
-                                 'SearchWords': ii['SearchWords'], \
-                                 'WikipediaLen': ii['WikipediaLen'] })
-                features_train_file.seek(0)
+                                     'Sentence Position': ii['Sentence Position'], \
+                                     'Answer': ii['Answer'], \
+                                     'is correct': bb['is correct'], \
+                                     'Q score': bb['Q score'], \
+                                     'IR score': bb['IR score'], \
+                                     'join score': bb['join score'], \
+                                     'category': bb['category'], \
+                                     'Wikipedia Score': ii['Wikipedia Score'], \
+                                     'Wikipedia Found': ii['Wikipedia Found'], \
+                                     'SearchWords': ii['SearchWords'], \
+                                     'SearchFound': ii['SearchFound'], \
+                                     'WikipediaLen': ii['WikipediaLen'] })
+                break;
+        features_train_file.seek(0)
                 #print "MATCH: ", ii['Question ID'], ii['Sentence Position'], ii['Answer'], bb['Q score'], bb['IR score'], bb['join score'],bb['category'],ii['Wikipedia Score'],ii['Wikipedia Found'],ii['SearchWords'],ii['SearchFound'],ii['WikipediaLen']
                 #print ''
 
