b0a04c55ac96f7314e5aef5089600b69a09d5178
Anas Salamah <anassalamah@Anass-iMac.local>
2014-12-14 11:14:54 -0700

    clean up and all_info.csv and Q_info_test.csv

---
+++ linear regressor/merge_wiki_long_and_features_train.py
@@ -0,0 +1,36 @@
+import sys
+from csv import DictReader, DictWriter
+
+if __name__ == "__main__":
+
+    print "sorting and merging wiki_longfeatures_train.csv and features_train.csv"
+
+    # Read in files
+    wiki_long= DictReader(open("wiki_longfeatures_train.csv", 'rU'))
+    features_train_file = open("features_train.csv", 'rU')
+    features_train = DictReader(features_train_file)
+    
+    # Create a File for output
+    output = DictWriter(open('wiki_long_features_train.csv', 'w'), ['Question ID', 'Sentence Position', 'Answer', 'is correct', 'Q score','IR score','join score', 'category','Wikipedia Score', 'Wikipedia Found', 'SearchWords','SearchFound', 'WikipediaLen'], lineterminator='\n')
+    output.writeheader()
+
+    for ii in wiki_long:
+        for bb in features_train:
+            if (ii['Question ID'],ii['Sentence Position'],ii['Answer']) == ( bb['Question ID'],bb['Sentence Position'],bb['Answer']):
+                output.writerow({'Question ID': ii['Question ID'], \
+                                 'Sentence Position': ii['Sentence Position'], \
+                                 'Answer': ii['Answer'], \
+                                 'is correct': bb['is correct'], \
+                                 'Q score': bb['Q score'], \
+                                 'IR score': bb['IR score'], \
+                                 'join score': bb['join score'], \
+                                 'category': bb['category'], \
+                                 'Wikipedia Score': ii['Wikipedia Score'], \
+                                 'Wikipedia Found': ii['Wikipedia Found'], \
+                                 'SearchWords': ii['SearchWords'], \
+                                 'WikipediaLen': ii['WikipediaLen'] })
+                features_train_file.seek(0)
+                #print "MATCH: ", ii['Question ID'], ii['Sentence Position'], ii['Answer'], bb['Q score'], bb['IR score'], bb['join score'],bb['category'],ii['Wikipedia Score'],ii['Wikipedia Found'],ii['SearchWords'],ii['SearchFound'],ii['WikipediaLen']
+                #print ''
+
+    
